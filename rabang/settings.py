cfee7edfd0565253bc174ce7b89b0caa649b7472
cuhong <hongcoilhouse@gmail.com>
2021-01-01 13:13:40 +0900

    add channels

--- rabang/settings.py
+++ rabang/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'channels',
+    'channels_redis',
     'simple_history',
     'ordered_model',
     'solo',
